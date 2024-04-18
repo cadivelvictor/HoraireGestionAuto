2089cd54826fac6bb9409af0b3e67fbfa81506ce
Victor CADIVEL <cadivelvictor@hotmail.com>
2024-04-18 16:28:00 +0400

    Modifications du 18/04/2024 16h27

--- ProtoThermometre/Thermometre.cs
+++ ProtoThermometre/Thermometre.cs
@@ -25,6 +25,7 @@ namespace ProtoThermometre
         private double tpEtiquetteCorrespondance = 12;
         private double tpEtiquetteNoLigne = 12;
         private RotateTransform rotationEtiquetteNomArretTerminus = new RotateTransform(0);
+        private double distnacePANA = 230;       //Définit la distance entre le point du premier/dernier arret et l'étiquette du nom du premier/dernier arret
 
 
         public Thermometre(string couleurLigne, string couleurPoleEchange, List<Arret> lesArrets, List<Ligne> lesLignes)
@@ -68,17 +69,20 @@ namespace ProtoThermometre
 
         public void Construire(Canvas canvas)
         {
-            double abscisseArc = canvas.Width / 2;
-            double ordonneeArc = canvas.Height / 2;
+            //Affiche 21
+            //MessageBox.Show(LesArrets.Count.ToString());
 
-            Line arcPrincipal = CreerArc(0, 0, 500, 0, CouleurLigne, 2);
-            canvas.Children.Add(arcPrincipal);
+            //Line arcPrincipal = CreerAxe(0, (canvas.Height/2), canvas.Width, (canvas.Height/2), CouleurLigne, 2);
+            Line axePrincipal = CreerAxe(0, (canvas.Height / 2), canvas.Width, (canvas.Height / 2), CouleurLigne, 2);
+            canvas.Children.Add(axePrincipal);
 
-            double xPAPremier = arcPrincipal.X1 - LongueurPointArret / 2;
-            double yPAPremier = arcPrincipal.Y1 - HauteurPointArret / 2;
+            double xPAPremier = axePrincipal.X1 - (LongueurPointArret / 2);
+            double yPAPremier = axePrincipal.Y1 - (HauteurPointArret / 2);
 
-            double xPADernier = arcPrincipal.X2 - LongueurPointArret / 2;
-            double yPADernier = arcPrincipal.Y2 - HauteurPointArret / 2;
+            double xPADernier = axePrincipal.X2 - (LongueurPointArret / 2);
+            double yPADernier = axePrincipal.Y2 - (HauteurPointArret / 2);
+
+
 
             if (LesArrets.Count > 0)
             {
@@ -87,6 +91,8 @@ namespace ProtoThermometre
 
                 if(premierArret != null)
                 {
+
+
                     if (premierArret.GetEstCorrespondance)
                     {
                         Ellipse pointArret = CreerPointArret(LongueurPointArret, HauteurPointArret, CouleurPoleEchange, EpaisseurTraitArret, premierArret.GetNom);
@@ -94,15 +100,22 @@ namespace ProtoThermometre
                         Label etiquetteCorrespondanceArret = CreerEtiquetteCorrespondance(tpEtiquetteCorrespondance, premierArret.GetEstPoleEchange);
 
                         etiquetteNomArret.RenderTransform = rotationEtiquetteNomArretTerminus;
+                        etiquetteNomArret.FontSize = 20;
 
                         Canvas.SetLeft(pointArret, xPAPremier);
                         Canvas.SetTop(pointArret, yPAPremier);
 
-                        Canvas.SetLeft(etiquetteNomArret, xPAPremier + 5);
-                        Canvas.SetTop(etiquetteNomArret, yPAPremier - 230);
+                        Canvas.SetLeft(etiquetteNomArret, axePrincipal.X1);
+                        Canvas.SetTop(etiquetteNomArret, (axePrincipal.Y1 - distnacePANA));
 
-                        Canvas.SetLeft(etiquetteCorrespondanceArret, xPAPremier - 2);
-                        Canvas.SetTop(etiquetteCorrespondanceArret, yPAPremier - 6);
+                        Canvas.SetLeft(etiquetteCorrespondanceArret, (xPAPremier - 2));
+                        Canvas.SetTop(etiquetteCorrespondanceArret, (yPAPremier - 6));
+
+                        Line axe1TermDebut = CreerAxe(axePrincipal.X1, (canvas.Height / 2), axePrincipal.X1, (Canvas.GetTop(etiquetteNomArret) + etiquetteNomArret.FontSize * 2), CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe1TermDebut);
+
+                        Line axe2TermDebut = CreerAxe((axe1TermDebut.X2 - 1), axe1TermDebut.Y2, (etiquetteNomArret.ToString().Length * 2), axe1TermDebut.Y2, CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe2TermDebut);
 
                         canvas.Children.Add(pointArret);
                         canvas.Children.Add(etiquetteNomArret);
@@ -116,15 +129,22 @@ namespace ProtoThermometre
                         Label etiquetteCorrespondancePoleEchange = CreerEtiquetteCorrespondance(tpEtiquetteCorrespondance, premierArret.GetEstPoleEchange);
 
                         etiquetteNomPoleEchange.RenderTransform = rotationEtiquetteNomArretTerminus;
+                        etiquetteNomPoleEchange.FontSize = 20;
 
                         Canvas.SetLeft(pointPoleEchange, xPAPremier);
                         Canvas.SetTop(pointPoleEchange, yPAPremier);
 
-                        Canvas.SetLeft(etiquetteNomPoleEchange, xPAPremier + 5);
-                        Canvas.SetTop(etiquetteNomPoleEchange, yPAPremier - 230);
+                        Canvas.SetLeft(etiquetteNomPoleEchange, axePrincipal.X1);
+                        Canvas.SetTop(etiquetteNomPoleEchange, (axePrincipal.Y1 - distnacePANA));
 
-                        Canvas.SetLeft(etiquetteCorrespondancePoleEchange, xPAPremier - 2);
-                        Canvas.SetTop(etiquetteCorrespondancePoleEchange, yPAPremier - 6);
+                        Canvas.SetLeft(etiquetteCorrespondancePoleEchange, (xPAPremier - 2));
+                        Canvas.SetTop(etiquetteCorrespondancePoleEchange, (yPAPremier - 6));
+
+                        Line axe1TermDebut = CreerAxe(axePrincipal.X1, (canvas.Height / 2), axePrincipal.X1, (Canvas.GetTop(etiquetteNomPoleEchange) + etiquetteNomPoleEchange.FontSize * 2), CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe1TermDebut);
+
+                        Line axe2TermDebut = CreerAxe((axe1TermDebut.X2 - 1), axe1TermDebut.Y2, (etiquetteNomPoleEchange.ToString().Length * 2), axe1TermDebut.Y2, CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe2TermDebut);
 
                         canvas.Children.Add(pointPoleEchange);
                         canvas.Children.Add(etiquetteNomPoleEchange);
@@ -136,12 +156,19 @@ namespace ProtoThermometre
                         Label etiquetteNomArret = CreerEtiquetteNomArret(premierArret.GetNom, tpEtiquetteNomArret, premierArret.GetEstPoleEchange);
 
                         etiquetteNomArret.RenderTransform = rotationEtiquetteNomArretTerminus;
+                        etiquetteNomArret.FontSize = 20;
 
                         Canvas.SetLeft(pointArret, xPAPremier);
                         Canvas.SetTop(pointArret, yPAPremier);
 
-                        Canvas.SetLeft(etiquetteNomArret, xPAPremier + 5);
-                        Canvas.SetTop(etiquetteNomArret, yPAPremier - 230);
+                        Canvas.SetLeft(etiquetteNomArret, axePrincipal.X1);
+                        Canvas.SetTop(etiquetteNomArret, (axePrincipal.Y1 - distnacePANA));
+
+                        Line axe1TermDebut = CreerAxe(axePrincipal.X1, (canvas.Height / 2), axePrincipal.X1, (Canvas.GetTop(etiquetteNomArret) + etiquetteNomArret.FontSize * 2), CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe1TermDebut);
+
+                        Line axe2TermDebut = CreerAxe((axe1TermDebut.X2 - 1), axe1TermDebut.Y2, (etiquetteNomArret.ToString().Length * 2), axe1TermDebut.Y2, CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe2TermDebut);
 
                         canvas.Children.Add(pointArret);
                         canvas.Children.Add(etiquetteNomArret);
@@ -159,15 +186,22 @@ namespace ProtoThermometre
                         Label etiquetteCorrespondanceArret = CreerEtiquetteCorrespondance(tpEtiquetteCorrespondance, dernierArret.GetEstPoleEchange);
 
                         etiquetteNomArret.RenderTransform = rotationEtiquetteNomArretTerminus;
+                        etiquetteNomArret.FontSize = 20;
 
                         Canvas.SetLeft(pointArret, xPADernier);
                         Canvas.SetTop(pointArret, yPADernier);
 
-                        Canvas.SetLeft(etiquetteNomArret, xPADernier - 225);
-                        Canvas.SetTop(etiquetteNomArret, yPADernier - 230);
+                        Canvas.SetRight(etiquetteNomArret, axePrincipal.X1);
+                        Canvas.SetTop(etiquetteNomArret, (axePrincipal.Y2 - distnacePANA)); 
 
-                        Canvas.SetLeft(etiquetteCorrespondanceArret, xPADernier - 2);
-                        Canvas.SetTop(etiquetteCorrespondanceArret, yPADernier - 6);
+                        Canvas.SetLeft(etiquetteCorrespondanceArret,(xPADernier - 2));
+                        Canvas.SetTop(etiquetteCorrespondanceArret, (yPADernier - 6));
+
+                        Line axe1TermFin = CreerAxe(axePrincipal.X2, (canvas.Height/2), axePrincipal.X2, (Canvas.GetTop(etiquetteNomArret) + etiquetteNomArret.FontSize * 2), CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe1TermFin);
+
+                        Line axe2TermFin = CreerAxe((axe1TermFin.X2 + 1), axe1TermFin.Y2, (etiquetteNomArret.ToString().Length * 2), axe1TermFin.Y2, CouleurPoleEchange, 2);
+                        canvas.Children.Add(axe2TermFin);
 
                         canvas.Children.Add(pointArret);
                         canvas.Children.Add(etiquetteNomArret);
@@ -181,15 +215,16 @@ namespace ProtoThermometre
                         Label etiquetteCorrespondancePoleEchange = CreerEtiquetteCorrespondance(tpEtiquetteCorrespondance, dernierArret.GetEstPoleEchange);
 
                         etiquetteNomPoleEchange.RenderTransform = rotationEtiquetteNomArretTerminus;
+                        etiquetteNomPoleEchange.FontSize = 20;
 
                         Canvas.SetLeft(pointPoleEchange, xPADernier);
                         Canvas.SetTop(pointPoleEchange, yPADernier);
 
-                        Canvas.SetLeft(etiquetteNomPoleEchange, xPADernier - 225);
-                        Canvas.SetTop(etiquetteNomPoleEchange, yPADernier - 230);
+                        Canvas.SetRight(etiquetteNomPoleEchange, axePrincipal.X1);
+                        Canvas.SetTop(etiquetteNomPoleEchange,(axePrincipal.Y2 - distnacePANA));
 
-                        Canvas.SetLeft(etiquetteCorrespondancePoleEchange, xPADernier - 2);
-                        Canvas.SetTop(etiquetteCorrespondancePoleEchange, yPADernier - 6);
+                        Canvas.SetLeft(etiquetteCorrespondancePoleEchange, (xPADernier - 2));
+                        Canvas.SetTop(etiquetteCorrespondancePoleEchange, (yPADernier - 6));
 
                         canvas.Children.Add(pointPoleEchange);
                         canvas.Children.Add(etiquetteNomPoleEchange);
@@ -201,12 +236,13 @@ namespace ProtoThermometre
                         Label etiquetteNomArret = CreerEtiquetteNomArret(dernierArret.GetNom, tpEtiquetteNomArret, dernierArret.GetEstPoleEchange);
 
                         etiquetteNomArret.RenderTransform = rotationEtiquetteNomArretTerminus;
+                        etiquetteNomArret.FontSize = 20;
 
                         Canvas.SetLeft(pointArret, xPADernier);
                         Canvas.SetTop(pointArret, yPADernier);
 
-                        Canvas.SetLeft(etiquetteNomArret, xPADernier - 225);
-                        Canvas.SetTop(etiquetteNomArret, yPADernier - 230);
+                        Canvas.SetLeft(etiquetteNomArret, axePrincipal.X1);
+                        Canvas.SetTop(etiquetteNomArret, (axePrincipal.Y2 - distnacePANA));
 
                         canvas.Children.Add(pointArret);
                         canvas.Children.Add(etiquetteNomArret);
@@ -215,10 +251,12 @@ namespace ProtoThermometre
                     LesArrets.RemoveAt(LesArrets.Count - 1);
                 }                
             }
+
+
         }
 
 
-        private Line CreerArc(double x1, double y1, double x2, double y2, string couleur, double epaisseurTrait)
+        private Line CreerAxe(double x1, double y1, double x2, double y2, string couleur, double epaisseurTrait)
         {
             return new Line
             {
